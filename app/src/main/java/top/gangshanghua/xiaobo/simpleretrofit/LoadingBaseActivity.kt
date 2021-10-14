225999a12d8959d2aef8d016c9890d1d883fc52b
moxiaobo <moxiaobo@cmii.chinamobile.com>
2021-10-14 21:03:55 +0800

    fix bug: two requests over in the background, the loading will not be hidden. for the liveData can only send the last value out.

--- app/src/main/java/top/gangshanghua/xiaobo/simpleretrofit/LoadingBaseActivity.kt
+++ app/src/main/java/top/gangshanghua/xiaobo/simpleretrofit/LoadingBaseActivity.kt
@@ -6,13 +6,30 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.activity.addCallback
 import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
 import com.blankj.utilcode.util.LogUtils
+import okhttp3.Call
 import top.gangshanghua.xiaobo.simpleretrofit.simple.SimpleApi
 
 abstract class LoadingBaseActivity : BaseActivity() {
 
     private lateinit var mProgressBar: ProgressBar
     private var mCount = 0
+    private val mLoadingObserver = Observer<Pair<Boolean, Call>> {
+        LogUtils.d("mCount before: $mCount")
+
+        if (it.first) {
+            if (mCount++ == 0) {
+                mProgressBar.isVisible = true
+            }
+        } else {
+            if (--mCount == 0) {
+                mProgressBar.isVisible = false
+            }
+        }
+
+        LogUtils.d("mCount after: $mCount")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,21 +49,12 @@ abstract class LoadingBaseActivity : BaseActivity() {
             }
         }
 
-        SimpleApi.mLoadingLiveData.observe(this) {
-            LogUtils.d("mCount before: $mCount")
+        SimpleApi.mLoadingLiveData.observeForever(mLoadingObserver)
+    }
 
-            if (it.first) {
-                if (mCount++ == 0) {
-                    mProgressBar.isVisible = true
-                }
-            } else {
-                if (--mCount == 0) {
-                    mProgressBar.isVisible = false
-                }
-            }
-
-            LogUtils.d("mCount after: $mCount")
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        SimpleApi.mLoadingLiveData.removeObserver(mLoadingObserver)
     }
 
 }
