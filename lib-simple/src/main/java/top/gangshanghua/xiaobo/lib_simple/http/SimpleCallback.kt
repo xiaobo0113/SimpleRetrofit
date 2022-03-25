ebb26032f68797b928a2b5eca92b286f0833bd1d
moxiaobo <moxiaobo@cmii.chinamobile.com>
2022-03-25 09:14:36 +0800

    isSuccessful() 为 true 的情况下，body 也可能为空

--- lib-simple/src/main/java/top/gangshanghua/xiaobo/lib_simple/http/SimpleCallback.kt
+++ lib-simple/src/main/java/top/gangshanghua/xiaobo/lib_simple/http/SimpleCallback.kt
@@ -11,16 +11,15 @@ class IgnoreResponseSimpleCallback<T> : SimpleCallback<T>(MutableLiveData<T?>())
 open class SimpleCallback<T>(private val mLiveData: MutableLiveData<T?>) : Callback<Result<T?>> {
 
     override fun onResponse(call: Call<Result<T?>>, response: Response<Result<T?>>) {
-        if (response.isSuccessful) {
-            // when isSuccessful is true, body() must not be null
-            val result = response.body()!!
+        val result = response.body()
+        if (result != null) {
             if (result.code != 200 && !result.message.isNullOrEmpty()) {
                 ToastUtils.showShort(result.message)
             }
 
             mLiveData.value = result.data
         } else {
-            // fail: such as 404/500, show tips
+            // eg: such as 404/500 or 204/205
             ToastUtils.showShort(response.message())
             mLiveData.value = null
         }
